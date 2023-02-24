91543e4873a736851263e147939db0577903463d
kshitijp <pawarkshitij888@gmail.com>
2023-02-24 20:06:09 +0530

    add product delete button

--- src/pages/products/[id]/index.tsx
+++ src/pages/products/[id]/index.tsx
@@ -10,6 +10,10 @@ import PageWithFallback from "../../../components/PageWithFallback";
 import Image from "../../../components/Image";
 import ExpandableText from "../../../components/ExpandableText";
 import Button from "../../../components/Button";
+import { useSession } from "next-auth/react";
+import StyledDialog from "../../../components/StyledDialog";
+import { useState } from "react";
+import { api } from "../../../utils/api";
 
 export const getStaticProps: GetStaticProps<{
   product:
@@ -43,13 +47,62 @@ export const getStaticPaths: GetStaticPaths = () => {
   };
 };
 
+const ProductDeleteDialog = ({ productId }: { productId: string }) => {
+  const [open, setOpen] = useState(false);
+
+  const { mutate, isLoading } = api.product.delete.useMutation({
+    onSuccess: () => {
+      //To do throw a toast here
+      console.log("Deleted");
+      setOpen(false);
+    },
+  });
+
+  const handleDeleteClick = () => {
+    if (isLoading) {
+      return;
+    }
+
+    mutate({ id: productId });
+  };
+
+  return (
+    <StyledDialog
+      open={open}
+      setOpen={setOpen}
+      Opener={<Button variants={{ type: "danger" }}>Delete this</Button>}
+      title="Delete this product"
+      description="Are you sure you want to do this ?"
+    >
+      <div className="mt-4 flex items-center gap-2 md:gap-4">
+        <Button variants={{ type: "secondary" }} onClick={() => setOpen(false)}>
+          No go back
+        </Button>
+        <Button
+          onClick={handleDeleteClick}
+          disabled={isLoading}
+          variants={{ type: "danger" }}
+        >
+          Yes delete this
+        </Button>
+      </div>
+    </StyledDialog>
+  );
+};
+
 const ProductPage = ({
   product,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const { data } = useSession();
+
   if (!product) {
     //To do show error page here
     return <div>Failed to get product</div>;
   }
+
+  const isAuthedToEdit = product.userId === data?.user?.id;
+
+  console.log({ isAuthedToEdit });
 
   return (
     <div className="flex w-full flex-col gap-4 p-5 text-zinc-300 md:p-8 xl:flex-row xl:gap-8">
@@ -77,6 +130,9 @@ const ProductPage = ({
         <div className="flex gap-2">
           <Button variants={{ type: "secondary" }}>Add to cart</Button>
           <Button>Buy now</Button>
+          {isAuthedToEdit ? (
+            <ProductDeleteDialog productId={product.id} />
+          ) : null}
         </div>
       </div>
 
