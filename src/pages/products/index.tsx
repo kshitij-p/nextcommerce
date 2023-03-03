bcb4d2efa9bfc04929f62d87a20c7befe5550f1f
kshitijp <pawarkshitij888@gmail.com>
2023-03-03 21:12:51 +0530

    add searchbar to products

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,6 +1,6 @@
 import { useSession } from "next-auth/react";
 import Link from "next/link";
-import React from "react";
+import React, { useMemo, useState } from "react";
 import Image from "../../components/Image";
 import TruncatedText from "../../components/TruncatedText";
 import { TIME_IN_MS } from "../../utils/client";
@@ -11,26 +11,39 @@ const AllProductsPage = () => {
   const { status } = useSession();
 
   const {
-    data: { products },
+    data: { products: allProducts },
   } = api.product.getAll.useQuery(undefined, {
     initialData: { message: "Initial data received", products: [] },
     initialDataUpdatedAt: 0,
     staleTime: TIME_IN_MS.FIVE_MINUTES,
   });
 
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const products = useMemo(() => {
+    return allProducts.filter((product) => product.title.includes(searchQuery));
+  }, [allProducts, searchQuery]);
+
   return (
-    <div className="flex flex-col items-center gap-2 p-4 md:gap-4 md:p-8">
-      <div className="ml-1 flex items-center gap-2 md:ml-2 xl:ml-3">
-        <h2 className="text-3xl font-semibold md:text-5xl">Products</h2>
-        {status === "authenticated" ? (
-          <ButtonLink
-            className="text-xl"
-            href={`/products/create`}
-            variants={{ type: "secondary" }}
-          >
-            Create
-          </ButtonLink>
-        ) : null}
+    <div className="flex flex-col items-center gap-2 p-4 md:gap-8 md:p-8">
+      <div className="flex flex-col gap-2">
+        <div className="ml-1 flex items-center gap-2 md:ml-2 xl:ml-3">
+          <h2 className="text-3xl font-semibold md:text-5xl">Products</h2>
+          {status === "authenticated" ? (
+            <ButtonLink
+              className="text-xl"
+              href={`/products/create`}
+              variants={{ type: "secondary" }}
+            >
+              Create
+            </ButtonLink>
+          ) : null}
+        </div>
+        <input
+          className="rounded p-1 md:p-2"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.currentTarget.value)}
+        />
       </div>
       <div className="flex w-full flex-col items-center gap-4 md:gap-8 xl:flex-row xl:flex-wrap">
         {products.map((product) => {
