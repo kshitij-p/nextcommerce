58dd97479b625ffab856c78e8a5fab85c056addd
kshitijp <pawarkshitij888@gmail.com>
2023-03-18 22:43:33 +0530

    add image sizes to cart page

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -8,7 +8,7 @@ import TruncatedText from "../components/ui/TruncatedText";
 import useEditCartQuantity from "../hooks/cart/useEditCartQuantity";
 import useTRPCUtils from "../hooks/useTRPCUtils";
 import { api, type RouterOutputs } from "../utils/api";
-import { TIME_IN_MS } from "../utils/client";
+import { breakpoints, TIME_IN_MS } from "../utils/client";
 import Head from "next/head";
 
 const RemoveFromCartButton = ({
@@ -82,8 +82,10 @@ const RemoveFromCartButton = ({
 
 const CartItem = ({
   cartItem,
+  index,
 }: {
   cartItem: RouterOutputs["cart"]["get"]["cart"]["cartItems"][0];
+  index: number;
 }) => {
   const { product, id, quantity } = cartItem;
 
@@ -100,69 +102,63 @@ const CartItem = ({
   };
 
   return (
-    <>
-      <Head>
-        <title>My cart | Nextcommerce</title>
-      </Head>
-      <div className="relative flex w-full gap-2 p-2 md:gap-4 xl:max-w-[50%]">
-        <Image
-          fill
-          Container={
-            <Link
-              tabIndex={-1}
-              href={`/products/${product.id}`}
-              prefetch={false}
-              className="w-36 shrink-0 md:w-56"
-            />
-          }
-          className="rounded-sm object-cover"
-          src={product.images[0]?.publicUrl ?? ""}
-          alt={`Image of ${product.title}`}
-        />
-        <div className="flex min-w-0 flex-col md:text-2xl">
-          <Link href={`/products/${product.id}`} prefetch={false}>
-            <TruncatedText
-              className="text-xl font-bold md:text-5xl"
-              title={product.title}
-              lineHeight={1}
-              maxLines={2}
-            >
-              {product.title}
-            </TruncatedText>
-          </Link>
-          <p>{`$${product.price}`}</p>
-          <TruncatedText maxLines={2} lineHeight={1}>
-            {product.description}
+    <div className="relative flex w-full gap-2 p-2 md:gap-4 xl:max-w-[50%]">
+      <Image
+        priority={index <= 2}
+        fill
+        Container={
+          <Link
+            tabIndex={-1}
+            href={`/products/${product.id}`}
+            prefetch={false}
+            className="w-36 shrink-0 md:w-56"
+          />
+        }
+        className="rounded-sm object-cover"
+        src={product.images[0]?.publicUrl ?? ""}
+        alt={`Image of ${product.title}`}
+        sizes={`(max-width: ${breakpoints.sm}): 144px, 224px`}
+      />
+      <div className="flex min-w-0 flex-col md:text-2xl">
+        <Link href={`/products/${product.id}`} prefetch={false}>
+          <TruncatedText
+            className="text-xl font-bold md:text-5xl"
+            title={product.title}
+            lineHeight={1}
+            maxLines={2}
+          >
+            {product.title}
           </TruncatedText>
-          <div className="flex gap-2">
-            <p>Qty: </p>
-            <div className="flex gap-1">
-              <button
-                onClick={async () => {
-                  await handleUpdateQuantity(-1);
-                }}
-              >
-                -
-              </button>
-              <p className="font-semibold">{quantity}</p>
-              <button
-                onClick={async () => {
-                  await handleUpdateQuantity(1);
-                }}
-              >
-                +
-              </button>
-            </div>
-          </div>
-          <div className="mt-auto mb-2">
-            <RemoveFromCartButton
-              cartItemId={id}
-              disabled={isLoadingQuantity}
-            />
+        </Link>
+        <p>{`$${product.price}`}</p>
+        <TruncatedText maxLines={2} lineHeight={1}>
+          {product.description}
+        </TruncatedText>
+        <div className="flex gap-2">
+          <p>Qty: </p>
+          <div className="flex gap-1">
+            <button
+              onClick={async () => {
+                await handleUpdateQuantity(-1);
+              }}
+            >
+              -
+            </button>
+            <p className="font-semibold">{quantity}</p>
+            <button
+              onClick={async () => {
+                await handleUpdateQuantity(1);
+              }}
+            >
+              +
+            </button>
           </div>
         </div>
+        <div className="mt-auto mb-2">
+          <RemoveFromCartButton cartItemId={id} disabled={isLoadingQuantity} />
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
@@ -194,29 +190,38 @@ const CartPage = () => {
   );
 
   return (
-    <div className="p-4 md:p-8">
-      <div className="mobile-scrollbar flex h-[70vh] w-full flex-col gap-1 overflow-auto">
-        {cart.cartItems.map((cartItem) => {
-          return <CartItem cartItem={cartItem} key={cartItem.id} />;
-        })}
-      </div>
-      <div className="flex w-full flex-col items-center">
-        <div className="text-md flex w-full max-w-xl flex-col items-center md:text-4xl">
-          <div className="flex w-full items-end justify-between gap-1 px-4">
-            <p>{`Total: `}</p>
-            <b>{`$${totalPrice}`}</b>
+    <>
+      <Head>
+        <title>My cart | Nextcommerce</title>
+      </Head>
+      <div className="p-4 md:p-8">
+        <div className="mobile-scrollbar flex h-[70vh] w-full flex-col gap-1 overflow-auto">
+          {cart.cartItems.map((cartItem, idx) => {
+            return (
+              <CartItem cartItem={cartItem} key={cartItem.id} index={idx} />
+            );
+          })}
+        </div>
+        <div className="flex w-full flex-col items-center">
+          <div className="text-md flex w-full max-w-xl flex-col items-center md:text-4xl">
+            <div className="flex w-full items-end justify-between gap-1 px-4">
+              <p>{`Total: `}</p>
+              <b>{`$${totalPrice}`}</b>
+            </div>
+            <Divider
+              className="my-2 bg-neutral-800"
+              thickness="1px"
+              size="100%"
+            />
           </div>
-          <Divider
-            className="my-2 bg-neutral-800"
-            thickness="1px"
-            size="100%"
-          />
-        </div>
-        <div className="mt-2">
-          <Button variants={{ type: "secondary", size: "lg" }}>Checkout</Button>
+          <div className="mt-2">
+            <Button variants={{ type: "secondary", size: "lg" }}>
+              Checkout
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
