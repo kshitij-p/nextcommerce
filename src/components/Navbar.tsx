418cc7f9837473a51cddb50b0be38bd65c8dcacb
kshitijp <pawarkshitij888@gmail.com>
2023-03-26 21:36:15 +0530

    change styling of navbar on xl screen sizes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -50,25 +50,29 @@ const Navbar = () => {
   return (
     <header className="sticky inset-0 z-[1300] bg-neutral-1000/50 backdrop-blur-[6px]">
       <nav className="flex items-center justify-center gap-4 p-2 text-lg font-light tracking-wider md:gap-12 md:p-4 md:text-3xl xl:gap-16 [&>*]:shrink-0">
-        <Link className="text-xl font-bold md:text-4xl" href="/">
-          NEXT
-        </Link>
+        <div className="flex items-center justify-center gap-[inherit] xl:w-full xl:justify-around [&>*]:shrink-0">
+          <Link className="text-xl font-bold md:text-4xl" href="/">
+            NEXT
+          </Link>
 
-        <Link
-          className="underline-teal-anim focus:outline focus:outline-2 focus:outline-teal-500"
-          href="/cart"
-        >
-          Cart
-        </Link>
+          <div className="flex items-center justify-center gap-[inherit] [&>*]:shrink-0">
+            <Link
+              className="underline-teal-anim focus:outline focus:outline-2 focus:outline-teal-500"
+              href="/products"
+              prefetch={false}
+            >
+              Products
+            </Link>
 
-        <Link
-          className="underline-teal-anim focus:outline focus:outline-2 focus:outline-teal-500"
-          href="/products"
-          prefetch={false}
-        >
-          Products
-        </Link>
-        <AuthButton className="hidden xl:inline-block" />
+            <Link
+              className="underline-teal-anim focus:outline focus:outline-2 focus:outline-teal-500"
+              href="/cart"
+            >
+              Cart
+            </Link>
+            <AuthButton className="hidden xl:inline-block" />
+          </div>
+        </div>
         <Drawer
           Container={
             <div className="h-full w-full bg-teal-900/10 p-8 text-3xl backdrop-blur md:p-14 md:text-6xl" />
